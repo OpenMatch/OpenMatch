cb6b2e6ed9b56f5b276d93da3337d3601b492263
Shi Yu <yushi17@foxmail.com>
2022-10-20 10:32:49 +0800

    rename script folder

--- scripts/ANCETele/shells/download_model.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p $PLM_DIR
-cd $PLM_DIR
-
-wget -c https://huggingface.co/Luyu/co-condenser-marco/resolve/main/pytorch_model.bin
-wget -c https://huggingface.co/Luyu/co-condenser-marco/resolve/main/tokenizer_config.json
-wget -c https://huggingface.co/Luyu/co-condenser-marco/resolve/main/vocab.txt
-wget -c https://huggingface.co/Luyu/co-condenser-marco/resolve/main/special_tokens_map.json
-wget -c https://huggingface.co/Luyu/co-condenser-marco/resolve/main/config.json
